954073ab3bcc4d314ea1a11f114c146c3d352f9b
Conniesy2021 <77601635+Conniesy2021@users.noreply.github.com>
2021-03-25 16:21:56 -0500

    aws s3 cp s3://wildrydes-us-east-1/WebApplication/1_StaticWebHosting/website ./ --recursive
    aws s3 cp s3://wildrydes-us-east-1/WebApplication/1_StaticWebHosting/website ./ --recursive

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_P6eHbuMOe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7th965l2e1dmani8f6d63ifsa1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://z2frc14vli.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
