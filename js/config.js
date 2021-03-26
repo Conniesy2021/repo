a78bc2ccc3596b57c37285bc45074b75065bd929
Conniesy2021 <77601635+Conniesy2021@users.noreply.github.com>
2021-03-25 19:41:39 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_P6eHbuMOe', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7th965l2e1dmani8f6d63ifsa1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-central-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_cvtXyGAuQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7gol1f9g0bhrrrs7rrl8lm9rg3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://z2frc14vli.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
